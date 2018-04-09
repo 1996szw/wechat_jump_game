bbbd451583d23ff403ad39443d1bc729d6875d93
1996szw <1445427773@qq.com>
2018-04-09 15:07:10 +0800

    submit

--- wechat_jump_auto_iOS.py
+++ wechat_jump_auto_iOS.py
@@ -59,15 +59,15 @@ def pull_screenshot():
     c.screenshot('1.png')
 
 
-def jump(distance):
-    press_time = distance * time_coefficient / 1000
-    print('press time: {}'.format(press_time))
-    s.tap_hold(200, 200, press_time)
-
 # def jump(distance):
 #     press_time = distance * time_coefficient / 1000
 #     print('press time: {}'.format(press_time))
-#     s.tap_hold(random.uniform(0, 500), random.uniform(0, 500), press_time)
+#     s.tap_hold(200, 200, press_time)
+
+def jump(distance):
+    press_time = distance * time_coefficient / 1000
+    print('press time: {}'.format(press_time))
+    s.tap_hold(random.uniform(0, 500), random.uniform(0, 500), press_time)
 
 def backup_screenshot(ts):
     """
